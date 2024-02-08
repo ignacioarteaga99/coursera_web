77aa95607a7fb1c9b4ecda79b3128e0198dcef00
iarteaga99 <ignacio.arteaga@uc.cl>
2024-02-08 17:18:50 -0300

    update

--- module5-solution/js/script.js
+++ module5-solution/js/script.js
@@ -83,8 +83,9 @@ $(function () { // Same as document.addEventListener("DOMContentLoaded"...
     showLoading("#main-content");
     $ajaxUtils.sendGetRequest(
       allCategoriesUrl,
-      function (event) {// ***** <---- TODO: STEP 1: Substitute [...] ******
-        buildAndShowHomeHTML(event);
+      function (responseText) {// ***** <---- TODO: STEP 1: Substitute [...] ******
+        var categories = JSON.parse(responseText);
+        buildAndShowHomeHTML(categories);
       },
       true); // Explicitly setting the flag to get JSON from server processed into an object literal
   });
@@ -103,8 +104,7 @@ $(function () { // Same as document.addEventListener("DOMContentLoaded"...
         // TODO: STEP 2: Here, call chooseRandomCategory, passing it retrieved 'categories'
         // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
         // variable's name implies it expects.
-        var chosenCategoryShortName = {};
-        var chosenCategory = chooseRandomCategory(categories);
+        var chosenCategoryShortName = chooseRandomCategory(categories).short_name;
 
         // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
         // chosen category from STEP 2. Use existing insertProperty function for that purpose.
@@ -118,7 +118,7 @@ $(function () { // Same as document.addEventListener("DOMContentLoaded"...
         // it into the home html snippet.
         //.
 
-        var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml, "randomCategoryShortName", chosenCategory.short_name);
+        var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml, "randomCategoryShortName", chosenCategoryShortName);
 
         // TODO: STEP 4: Insert the produced HTML in STEP 3 into the main page
         // Use the existing insertHtml function for that purpose. Look through this code for an example
